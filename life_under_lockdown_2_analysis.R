8c1ce8690f96e1f479f95f8f995976e1a041774d
katie-ross <katherine.ross@kcl.ac.uk>
2024-02-02 17:46:03 +0000

    analysis work

--- life_under_lockdown_2_analysis.R
+++ life_under_lockdown_2_analysis.R
@@ -2,3 +2,9 @@ library(dplyr)
 library(readr)
 
 lul2_d <- read_csv('life-under-lockdown-wave2-extract.csv')
+
+lul2_d %>%
+  group_by(know_facebook) %>% 
+  summarise(n = n(), beliefs = mean(beliefs))
+  
+  
